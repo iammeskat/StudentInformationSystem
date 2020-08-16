70d3411e229f093e1a89056ec0b665c126365c1f
Meskatul Islam <meskatcse@gmail.com>
2020-08-16 22:47:28 +0600

    user registration created

--- app/Http/Controllers/Api/Students/UserController.php
+++ app/Http/Controllers/Api/Students/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Students;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use App\Models\User;
 
 class UserController extends Controller
@@ -12,21 +13,25 @@ class UserController extends Controller
     {
         
         #demo
+
         $validatedData = $request->validate([
         	'student_id' => 'required|max:13',
             'name' => 'required|max:55',
             'department' => 'required',
             'batch' => 'required',
+            'semester' => 'required',
             'advisor_id' => 'required|integer',
             'phone_number' => 'required|max:15|min:11',
             'email' => 'email|required|unique:users',
             'password' => 'required|confirmed'
         ]);
-        
-
+        #miss email_verified, email_verified_at, picture_path, status
         $validatedData['password'] = bcrypt($request->password);
+        $validatedData['email_verification_token'] = $request->email.Str::random(55);
+        $validatedData['status'] = 'inactive';
         $user = User::create($validatedData);
         $accessToken = $user->createToken('authToken')->accessToken;
+        $accessToken;
         return json_encode([
             'data'=>[
                 'user' => $user,
