3741f329768fe9026f17443488cbc4130d532f63
Meskatul Islam <meskatcse@gmail.com>
2020-08-17 00:37:11 +0600

    User Registration Validation Updated

--- app/Http/Controllers/Api/Students/UserController.php
+++ app/Http/Controllers/Api/Students/UserController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers\Api\Students;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+
+use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
-        
-        $validatedData = $request->validate([
-        	'student_id' => 'required|max:13',
+
+        $validator = Validator::make($request->all(), [
+            'student_id' => 'required|max:13',
             'name' => 'required|max:55',
             'department' => 'required',
             'batch' => 'required',
@@ -23,13 +25,29 @@ class UserController extends Controller
             'email' => 'email|required|unique:users',
             'password' => 'required|confirmed'
         ]);
+        if($validator->fails()){
+            return response()->json([
+                'message'=>'Validation Failed',
+                'errors'=>$validator->errors(),
+            ]);
+        }
         
-        $validatedData['password'] = bcrypt($request->password);
-        $validatedData['email_verification_token'] = $request->email.Str::random(55);
-        $validatedData['status'] = 'inactive';
-        $user = User::create($validatedData);
+        $user = User::create([
+            'student_id' => $request->student_id,
+            'name' => $request->name,
+            'department' => $request->department,
+            'batch' => $request->batch,
+            'semester' => $request->semester,
+            'advisor_id' => $request->advisor_id,
+            'phone_number' => $request->phone_number,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'email_verification_token' => $request->email.Str::random(55),
+            'status' => 'inactive',
+        ]);
+
         $accessToken = $user->createToken('authToken')->accessToken;
-        return json_encode([
+        return response()->json([
             'data'=>[
                 'user' => $user,
                 'access_token' => $accessToken
