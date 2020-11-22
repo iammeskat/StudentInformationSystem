049a87fbeaf894fb259c7a40bbe4e936d215834a
Meskatul Islam <meskatcse@gmail.com>
2020-11-23 01:09:19 +0600

    Post factory created

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit6c0a4ebfbbba320e35315122d0808502
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit6c0a4ebfbbba320e35315122d0808502', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit6c0a4ebfbbba320e35315122d0808502', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit6c0a4ebfbbba320e35315122d0808502::getInitializer($loader));
         } else {
