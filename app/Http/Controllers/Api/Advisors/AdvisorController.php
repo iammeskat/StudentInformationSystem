9fb375d9f8a3f4996231ff938776968ea80ac338
Meskatul Islam <meskatcse@gmail.com>
2020-11-24 22:42:05 +0600

    advisor controller

---
+++ app/Http/Controllers/Api/Advisors/AdvisorController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Advisors;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdvisorController extends Controller
+{
+    //
+}
