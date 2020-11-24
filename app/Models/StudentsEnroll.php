dddc05f23814fc6fd88c27f682dfa1ed33535ace
Meskatul Islam <meskatcse@gmail.com>
2020-11-24 21:15:13 +0600

    StudentsEnroll Model

--- app/Models/StudentsEnroll.php
+++ app/Models/StudentsEnroll.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class StudentsEnroll extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'student_id',
+        'course_id',
+        'session',
+        'semester',
+        'status',
+    ];
 }
